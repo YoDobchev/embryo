22258aac1b8d89c24b0b70e2f283774bb041552a
YoDobchev <yo.dobchev@gmail.com>
2021-11-30 22:57:25 +0200

    gotalicpita

---
+++ reveal.js-master/script.js
@@ -0,0 +1,13 @@
+function toggleText(fragment) {
+    switch (fragment) {
+        case "mass":
+            break
+        case "zona":
+            break
+        case "trophoblast":
+            break
+        case "blastocele":
+            d
+             break
+    }
+}
